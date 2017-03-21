d8116097e0a7b5fd90787548fdadc9027bcce82f
Mark Frömberg <mirque84@gmail.com>
2017-03-21 10:16:29 +0100

    Add new Plugin
    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Synced Tabs
+
+*This is a plugin for the [Glyphs font editor](http://glyphsapp.com/).*  Based on the script [Sync Edit Views.py](https://github.com/Tosche/Glyphs-Scripts/blob/master/Sync%20Edit%20Views.py) by [Tosche](https://github.com/Tosche)
+
+If active, it keeps all tabs from all open fonts in sync with the currently active tab.
+
+### How to use
+
+When ever you need it, toggle `Show Synced Tabs` from the view menu or the Roporter Toggler.
+
+### Examples
+
+-
+
+##### Known Issues
+
+- None so far
+
+##### TODO
+
+- selectedLayerOrigin [https://docu.glyphsapp.com/#selectedLayerOrigin]
+- Make the same check for placeholder Layer as it is implemented for the newLine (GSControlLayer)
+
+##### Pull Requests
+
+Feel free to comment or pull requests for any improvements.
+
+##### License
+
+Copyright 2017 [Mark Frömberg](http://www.markfromberg.com/) *@Mark2Mark*
+Collaboration with [Tosche](https://github.com/Tosche)
+
+Made possible with the GlyphsSDK by Georg Seifert (@schriftgestalt) and Rainer Erich Scheichelbauer (@mekkablue).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ SyncTool.glyphsReporter/Contents/Resources/plugin.py
@@ -0,0 +1,150 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Reporter Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Reporter
+#	
+#	Based on the script [Sync Edit Views.py](https://github.com/Tosche/Glyphs-Scripts/blob/master/Sync%20Edit%20Views.py) by [Tosche](https://github.com/Tosche)
+#	
+#	> Keep all tabs in all open fonts in sync with the currently active tab.
+#	> Set a /space in other tabs, when a certain glyph is not in these fonts.
+#	> Handles newline characters.
+#
+#	TODO: selectedLayerOrigin [https://docu.glyphsapp.com/#selectedLayerOrigin]
+#	TODO: Make the same check for placeholder Layer as it is implemented for the newLine (GSControlLayer)
+#
+#
+###########################################################################################################
+
+from GlyphsApp.plugins import *
+import traceback
+
+currentGlyphName = ""
+currentCaretPosition = None
+newLine = "\n"
+
+class SyncTool(ReporterPlugin):
+
+	def settings(self):
+		self.name = 'SyncTool'
+		self.menuName = Glyphs.localize({'en': u'Synced Tabs'})
+		#self.Glyphs = NSApplication.sharedApplication()
+		self.activeGlyphChanged = False
+
+
+	def SyncEditViews(self):
+		'''
+		Based on the code from Tosche : `Sync Edit Views.py` @Github
+		'''
+
+		try:
+			font0 = Glyphs.font
+			thisTab = font0.currentTab
+			thisMaster = font0.selectedFontMaster
+			mindex = thisTab.masterIndex()
+			thisScale = thisTab.graphicView().scale()
+			doKern = thisTab.graphicView().doKerning()
+			doSpace = thisTab.graphicView().doSpacing()
+			thisSelection = thisTab.graphicView().textStorage().selectedRange()
+
+			for otherFont in Glyphs.fonts:
+				if otherFont != Glyphs.font:
+					otherFontLastTool = otherFont.tool
+					iTab = otherFont.tabs[-1]
+					if mindex <= len(otherFont.masters):
+						iTab.setMasterIndex_(mindex)
+					otherView = iTab.graphicView()
+					otherView.setScale_(thisScale)
+					otherView.setDoKerning_(doKern)
+					otherView.setDoSpacing_(doSpace)
+					
+					## verify glyph in font
+					normalizedText, currentLayers = [], []
+					for l in thisTab.layers:
+						try:
+							currentLayers.append(l.parent.name)
+						except:
+							currentLayers.append(newLine)
+					for g in currentLayers: # for g in thisTab.text:
+						if g != newLine:
+							if g in otherFont.glyphs:
+								normalizedText.append(g)
+							else:
+								normalizedText.append("space")
+						else:
+							normalizedText.append(newLine)
+					
+					normalizedText = "/" + "/".join([x for x in normalizedText])
+					iTab.text = normalizedText
+
+					# SET CARET INTO POSITION
+					otherFont.tool = "TextTool" # switch to tt to trigger glyphs view to focus
+					otherView.textStorage().setSelectedRange_(thisSelection)
+					otherFont.tool = otherFontLastTool
+
+		except:
+			pass # print traceback.format_exc()
+
+
+	def observeGlyphChange(self):
+		'''
+		Catch the event: `active glyph was changed in the edit tab`.
+		Convert to str() !!! otherwise they are pyobj objects.
+
+		Calling the own layer here to make sure it is always the one from the active font.
+		Otherwise, if passing in the `layer` as argument, the observer will always take the
+		layer from each font during the loop. Hence it will endlessly loop into a glyphChangeObservation
+		when a glyph is selected that doesnt appear in the other fonts.
+		'''
+		global currentGlyphName
+		global currentCaretPosition
+
+		try:
+			layer = Glyphs.font.selectedLayers[0] #Glyphs.orderedDocuments()[0].font.selectedLayers[0]
+			lName = str(layer.parent.name)			
+			if str(currentGlyphName) != lName:
+				currentGlyphName = lName
+				self.activeGlyphChanged = True
+				return True
+			else:
+				### If same glyph, check if position in tab (Otherwise changing from one /b to another in one tab wont trigger)
+				position = layer.parent.parent.currentTab.graphicView().textStorage().selectedRange()
+				if currentCaretPosition != position:
+					currentCaretPosition = position
+					self.activeGlyphChanged = True
+					return True
+				### End: optional, but needed for this plugin
+
+				self.activeGlyphChanged = False
+				return False
+		except:
+			print traceback.format_exc()
+
+
+	def drawKammerakindRahmen(self, thisFont):
+		pass
+		'''
+		#sourceFontCurrentTab = Glyphs.font.currentTab
+		sourceFontCurrentTab = thisFont.currentTab
+		vp = sourceFontCurrentTab.viewPort
+		NSColor.colorWithCalibratedRed_green_blue_alpha_( 0, 0.8, 0.2, 0.1 ).set()
+		NSBezierPath.setLineWidth_(40)
+		NSBezierPath.strokeRect_(vp) # NSBezierPath.fillRect_(vp)
+		'''
+
+
+	def foregroundInViewCoords(self, layer): # important to make the viewport (in drawKammerakindRahmen) work.
+		self.observeGlyphChange()
+		if self.activeGlyphChanged:
+			self.SyncEditViews()
+
+		if layer.parent.parent == Glyphs.font:
+			self.drawKammerakindRahmen(layer.parent.parent)
+
+
+
+
